e996065e6491c376d32a3fe076374954b2beaff0
Dmytro20099 <chagayda08@gmail.com>
2024-05-11 12:47:35 +0300

    rehomework3

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def longest_string(first_str: str, second_str: str) -> str:
     return result
 
 
-def digit_list(text: list) -> bool:
+def digit_list(user_list: list) -> bool:
     """DIGIT_LIST return True if your text has only digits, else: False,
     example1: digit_list([3, 5, 7, 2, 13, 7]) -> True
-    example2: digit_list([6, 5, t, df, 7, 8, 3]) -> False"""
+    example2: digit_list([6, 5, 't', 'df', 7, 8, 3]) -> False"""
 
-    result = ""
-    for symbol in text:
+    result = True
+    for symbol in user_list:
         if str(symbol).isdigit():
             result = True
         else:
@@ -27,7 +27,10 @@ def digit_list(text: list) -> bool:
     return result
 
 
-def print_symbols():
+print(digit_list([6, 5, 't', 'df', 7, 8, 3]))
+
+
+def random_symbols():
     """PRINT_SYMBOLS print '/' % 50"""
 
     print("'/' % 50")
